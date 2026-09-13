8054f7a264d4cf71fb98293b858bbfa1e0daccd3
Hamisoptimistic <hamisoptimistic@users.noreply.github.com>
2026-09-14 04:01:06 +0530

    chore: add fluent reveal mouse tracking glow to network widget bottom border

--- src/MetroHub/Widgets/Catalog/Network/NetworkWidgetView.xaml.cs
+++ src/MetroHub/Widgets/Catalog/Network/NetworkWidgetView.xaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace MetroHub.Widgets.Catalog.Network;
 
@@ -7,5 +11,27 @@ public partial class NetworkWidgetView : UserControl
     public NetworkWidgetView()
     {
         InitializeComponent();
+    }
+
+    private void OnMouseMove(object sender, MouseEventArgs e)
+    {
+        var pos = e.GetPosition(this);
+        if (BottomRevealBrush != null)
+        {
+            BottomRevealBrush.Center = pos;
+            BottomRevealBrush.GradientOrigin = pos;
+        }
+    }
+
+    private void OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        var anim = new DoubleAnimation(1.0, TimeSpan.FromMilliseconds(100));
+        BottomRevealBorder?.BeginAnimation(UIElement.OpacityProperty, anim);
+    }
+
+    private void OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        var anim = new DoubleAnimation(0.0, TimeSpan.FromMilliseconds(200));
+        BottomRevealBorder?.BeginAnimation(UIElement.OpacityProperty, anim);
     }
 }
